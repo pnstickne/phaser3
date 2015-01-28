3497c4c32ddf6c68401f0c0edce42a9f998b35ed
Pete Baron <simoleste@gmail.com>
2015-01-28 17:43:43 +1300

    Implemented a multiple instance demo. Increases the number of rows or columns of game instances each time the fps sits at a steady 60 fps for five consecutive seconds.  My desktop handles 20 instances (5 x 4).

--- renderer/src/pbMultiCameraDemo.js
+++ renderer/src/pbMultiCameraDemo.js
@@ -7,42 +7,133 @@
 
 function pbMultiCameraDemo( docId )
 {
-	console.log( "pbAutoInvaderDemo c'tor entry" );
+	console.log( "pbMultiCameraDemo c'tor entry" );
 
 	var _this = this;
 	this.docId = docId;
 	
-	this.renderer = new pbRenderer( this.docId, this.create, this.update, this );
+	this.layer = null;
+	this.cameras = null;
+	this.fps60 = 0;
+
+	// create loader with callback when all items have finished loading
+	this.loader = new pbLoader( this.allLoaded, this );
+
+	this.playerImg = this.loader.loadImage( "../img/invader/player.png" );
+	this.invaderImg = this.loader.loadImage( "../img/invader/invader32x32x4.png" );
+	this.saucerImg = this.loader.loadImage( "../img/invader/invader.png" );
+	this.starsImg = this.loader.loadImage( "../img/invader/starfield.png" );
+	this.bulletImg = this.loader.loadImage( "../img/invader/bullet.png" );
+	this.bombImg = this.loader.loadImage( "../img/invader/enemy-bullet.png" );
+	this.rocketImg = this.loader.loadImage( "../img/invader/rockets32x32x8.png" );
+	this.smokeImg = this.loader.loadImage( "../img/invader/smoke64x64x8.png" );
+	this.explosionImg = this.loader.loadImage( "../img/invader/explode.png" );
+	this.fontImg = this.loader.loadImage( "../img/fonts/arcadeFonts/16x16/Bubble Memories (Taito).png" );
+
+	console.log( "pbMultiCameraDemo c'tor exit" );
 }
+
+
+pbMultiCameraDemo.prototype.allLoaded = function()
+{
+	console.log( "pbMultiCameraDemo.allLoaded" );
+
+	// callback to this.create when ready, callback to this.update once every frame
+	this.renderer = new pbRenderer( this.docId, this.create, this.update, this );
+};
 
 
 pbMultiCameraDemo.prototype.create = function()
 {
+	this.numWide = 1;
+	this.numHigh = 1;
+	this.addCameras();
+};
+
+
+pbMultiCameraDemo.prototype.addCameras = function()
+{
+	var cx = 0;
+	var cy = 0;
+	var tx = this.renderer.width / this.numWide;
+	var ty = this.renderer.height / this.numHigh;
+
 	this.cameras = [];
-	for(var i = 0; i < 4; i++)
+	for(var y = 0; y < this.numHigh; y++)
 	{
-		var layer = new pbLayer();
-		layer.create(rootLayer, this.renderer, 0, 0, 0, 0, 0.5, 0.5);
-		this.cameras[i] = new pbAutoInvaderDemo();
-		this.cameras[i].create(layer);
+		if (!this.cameras[y])
+			this.cameras[y] = [];
+
+		for(var x = 0; x < this.numWide; x++)
+		{
+			// add a new camera
+			var layer = new pbLayer();
+			layer.create(rootLayer, this.renderer, cx, cy, 0, 0, 1 / this.numWide, 1 / this.numHigh);
+			this.cameras[y][x] = new pbInvaderDemoCore();
+			this.cameras[y][x].create(this, layer);
+			rootLayer.addChild(layer);
+
+			cx += tx;
+		}
+		cx = 0;
+		cy += ty;
 	}
+};
+
+
+pbMultiCameraDemo.prototype.removeCameras = function()
+{
+	for(var y = 0; y < this.numHigh; y++)
+	{
+		for(var x = 0; x < this.numWide; x++)
+		{
+			this.cameras[y][x].layer.destroy();
+			this.cameras[y][x].destroy();
+		}
+		this.cameras[y] = null;
+	}
+	this.cameras = null;
 };
 
 
 pbMultiCameraDemo.prototype.destroy = function()
 {
-	for(var i = 0, l = this.cameras.length; i < l; i++)
-		this.cameras[i].destroy();
-	this.cameras = null;
+	this.removeCameras();
+
+	this.renderer.destroy();
+	this.renderer = null;
 };
 
 
 pbMultiCameraDemo.prototype.update = function()
 {
-	for(var i = 0, l = this.cameras.length; i < l; i++)
+	for(var y = 0; y < this.numHigh; y++)
+		for(var x = 0; x < this.numWide; x++)
+			this.cameras[y][x].update();
+
+	if (fps >= 60)
+		this.fps60++;
+	else
+		this.fps60 = 0;
+
+	if (this.fps60 > 60 * 5)
 	{
-		this.cameras[i].update();
+		this.fps60 = 0;
+		this.removeCameras();
+
+		if (this.numWide <= this.numHigh)
+			this.numWide++;
+		else
+			this.numHigh++;
+
+		this.addCameras();
 	}
 };
+
+
+
+
+
+
 
 
