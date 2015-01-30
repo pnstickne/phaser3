71df19895c20c5ae32eb8eb6865e334dbd51e9e6
Pete Baron <simoleste@gmail.com>
2015-01-31 02:08:48 +1300

    Attempt to speed up pbDictionary by removing indexOf searches and second array of keys.  Using keys as array accessors now.  No noticable improvement in speed but code is cleaner.

--- renderer/src/pbDictionary.js
+++ renderer/src/pbDictionary.js
@@ -2,8 +2,7 @@
  *
  * A very simple Dictionary implementation in JS
  *
- * Originally based on: http://jsfiddle.net/MickMalone1983/VEpFf/2/
- *
+ * Uses the key as a property on the values object.
  * Permits multiple values to be stored to a single key.
  * 
  */
@@ -11,7 +10,6 @@
 
 function pbDictionary()
 {
-	this.keys = null;
 	this.values = null;
 }
 
@@ -24,70 +22,65 @@ pbDictionary.prototype.create = function()
 
 pbDictionary.prototype.add = function(_key, _value)
 {
-	var i = this.keys.indexOf(_key);
-	if (i != -1)
+	var v = this.values[_key];
+	if (v === undefined || v === null)
 	{
-		// key exists, add value to the end of the list of matching values
-		this.values[i].push(_value);
+		// key does not exist, create new list of matching values
+		this.values[_key] = [ _value ];
 	}
 	else
 	{
-		// key does not exist, create new key and list of matching values
-		var i = this.keys.push(_key) - 1;
-		this.values[i] = [ _value ];
+		// key exists, add value to the end of the list of matching values
+		this.values[_key].push(_value);
 	}
 };
 
 
 pbDictionary.prototype.get = function(_key)
 {
-	var i = this.keys.indexOf(_key);
-	if (i != -1)
-	{
-		// key exists, return list of matching values
-		return this.values[i];
-	}
-
-	// key does not exist
-	return null;
+	var v = this.values[_key];
+	if (v === undefined || v === null)
+		// key does not exist
+		return null;
+	// key exists, return list of matching values
+	return this.values[_key];
 };
 
 
 pbDictionary.prototype.remove = function(_key)
 {
-	var i = this.keys.indexOf(_key);
-	if (i != -1)
-	{
-		// key exists, return list of matching values
-		var list = this.values[i];
-		this.keys[i] = null;
-		this.values[i] = null;
-		return list;
-	}
+	var v = this.values[_key];
+	if (v === undefined || v === null)
+		// key does not exist
+		return null;
 
-	// key does not exist
-	return null;
+	// key exists, return list of matching values
+	this.values[_key] = null;
+	return list;
 };
 
 
 pbDictionary.prototype.clear = function()
 {
-	this.keys = [];
 	this.values = [];
 };
 
 
 pbDictionary.prototype.iterateAll = function(_func, _context)
 {
-	for(var i = 0, l = this.keys.length; i < l; i++)
-		for(var j = 0, m = this.values[i].length; j < m; j++)
-			_func.call(_context, this.values[i][j]);
+	var v = this.values;
+	for(var k in v)
+		if (v.hasOwnProperty(k))
+			for(var j = 0, m = v[k].length; j < m; j++)
+				_func.call(_context, v[k][j]);
 };
 
 
 pbDictionary.prototype.iterateKeys = function(_func, _context)
 {
-	for(var i = 0, l = this.keys.length; i < l; i++)
-		_func.call(_context, this.values[i]);
+	var v = this.values;
+	for(var k in v)
+		if (v.hasOwnProperty(k))
+			_func.call(_context, v[k]);
 };
 
