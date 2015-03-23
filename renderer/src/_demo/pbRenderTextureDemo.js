8e22ceec25339ed48349d8e7119d750c7fcfbe22
Pete Baron <sibaroni@hotmail.com>
2015-03-23 15:28:02 +1300

    Tidy up and comments.

--- renderer/src/_demo/pbRenderTextureDemo.js
+++ renderer/src/_demo/pbRenderTextureDemo.js
@@ -105,7 +105,7 @@ pbRenderTextureDemo.prototype.addSprites = function()
 
     pbRenderTextureDemo.prototype.initTextureFramebuffer = function()
     {
-		// create an empty texture to draw to
+		// create an empty texture to draw to which matches the display dimensions
 		rttTexture = gl.createTexture();
         rttTexture.width = pbRenderer.width;
         rttTexture.height = pbRenderer.height;
@@ -115,19 +115,20 @@ pbRenderTextureDemo.prototype.addSprites = function()
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, rttTexture.width, rttTexture.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
-        //gl.generateMipmap(gl.TEXTURE_2D);
 
-        // attach the texture to the framebuffer
+        // attach the render-to-texture to a new framebuffer
 		rttFramebuffer = gl.createFramebuffer();
 		gl.bindFramebuffer(gl.FRAMEBUFFER, rttFramebuffer);
+        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, rttTexture, 0);
 
+		// create a 'render-to' depth buffer
         rttRenderbuffer = gl.createRenderbuffer();
         gl.bindRenderbuffer(gl.RENDERBUFFER, rttRenderbuffer);
         gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, rttTexture.width, rttTexture.height);
-
-        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, rttTexture, 0);
+        // attach the depth buffer to the framebuffer
         gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, rttRenderbuffer);
 
+        // clear the gl bindings
         gl.bindRenderbuffer(gl.RENDERBUFFER, null);
         gl.bindTexture(gl.TEXTURE_2D, null);
         gl.bindFramebuffer(gl.FRAMEBUFFER, null);
@@ -136,7 +137,14 @@ pbRenderTextureDemo.prototype.addSprites = function()
 
     pbRenderTextureDemo.prototype.drawSceneToTexture = function()
     {
+    	// bind the framebuffer so drawing will go to the associated texture and depth buffer
+		gl.bindFramebuffer(gl.FRAMEBUFFER, rttFramebuffer);
+		// clear the current texture using a varying green shade to make it stand out
+		gl.clearColor(0, (pbRenderer.frameCount % 100 / 100), 0, 1); // green shades;
+		gl.clear(gl.COLOR_BUFFER_BIT);
+    	// draw srcImage into the framebuffer texture
 		this.renderer.graphics.drawImageWithTransform(srcImage, this.transform, 1.0);
+		gl.bindFramebuffer(gl.FRAMEBUFFER, null);
     };
 
 
@@ -169,29 +177,27 @@ pbRenderTextureDemo.prototype.update = function()
 {
 	if (this.firstTime)
 	{
-		this.vertBuffer = gl.createBuffer();
-		gl.bindBuffer(gl.ARRAY_BUFFER, this.vertBuffer);
-		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
-		gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 0, 0);
-		gl.enableVertexAttribArray(0);
-		gl.bindBuffer(gl.ARRAY_BUFFER, null);
+		// create the render-to-texture, depth buffer, and a frame buffer to hold them
 		this.initTextureFramebuffer();
+
+		// create a buffer for the vertices used to transfer the render-to-texture to the display
+		this.vertBuffer = gl.createBuffer();
+
+		// set the transformation for rendering to the render-to-texture
 		this.transform = pbMatrix3.makeTransform(10, 10, 0, 1, 1);
+
+		// don't do this again...
 		this.firstTime = false;
 	}
 
-	gl.bindFramebuffer(gl.FRAMEBUFFER, rttFramebuffer);
-		// clear the texture using a varying green shade to make it stand out
-		gl.clearColor(0, (pbRenderer.frameCount % 100 / 100), 0, 1); // green shades;
-		gl.clear(gl.COLOR_BUFFER_BIT);
-		this.drawSceneToTexture();
-	gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+	// draw the srcImage to the render-to-texture
+	this.drawSceneToTexture();
 
 	// clear the display to red so we know when stuff is working...
-	gl.clearColor(1, 0, 0, 1); // red
+	gl.clearColor(1, 0, 0, 1);			// red
 	gl.clear(gl.COLOR_BUFFER_BIT);
 
-	// draw texture to the display
+	// draw the render-to-texture to the display
 	this.renderer.graphics.shaders.setProgram(this.renderer.graphics.shaders.simpleShaderProgram);
 	gl.bindTexture(gl.TEXTURE_2D, rttTexture);
 	gl.bindBuffer(gl.ARRAY_BUFFER, this.vertBuffer);
