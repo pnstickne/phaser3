b461e8e77bd071b209192f66cd7f1d67c61d7dfb
Pete Baron <sibaroni@hotmail.com>
2015-04-07 11:11:45 +1200

    First working version of the camera-render-to-texture demo.  The invaders, bullets, and smoke trails are not erasing currently (even though the starfield is scrolling, indicating it should be drawing over old data each frame).  I currently suspect that the data being drawn is increasing constantly and will hunt for a buffer not being cleared.

--- renderer/src/_demo/pbCameraRTTDemo.js
+++ renderer/src/_demo/pbCameraRTTDemo.js
@@ -79,45 +79,80 @@ pbCameraRTTDemo.prototype.destroy = function()
 };
 
 
-pbCameraRTTDemo.prototype.drawSceneToTexture = function(_fb)
-{
-	// bind the framebuffer so drawing will go to the associated texture and depth buffer
-	gl.bindFramebuffer(gl.FRAMEBUFFER, _fb);
-
-	// update the invaders demo core, drawing to the render-to-texture
-	this.game.update();
-};
-
-
 pbCameraRTTDemo.prototype.update = function()
 {
 	if (this.firstTime)
 	{
 		// create the render-to-texture, depth buffer, and a frame buffer to hold them
-		this.rttTexture = pbWebGlTextures.initTexture(gl.TEXTURE0, pbRenderer.width, pbRenderer.height);
+		this.rttTexture = pbWebGlTextures.initTexture(gl.TEXTURE0, pbRenderer.width, pbRenderer.height);	//512, 512);
 		this.rttRenderbuffer = pbWebGlTextures.initDepth(this.rttTexture);
 		this.rttFramebuffer = pbWebGlTextures.initFramebuffer(this.rttTexture, this.rttRenderbuffer);
 
-		// create the RAM surface to download the texture to
+		// create a RAM surface to download the texture to
 		this.textureSurface = new pbSurface();
 		// _wide, _high, _numWide, _numHigh, _image
 		this.textureSurface.create(this.rttTexture.width, this.rttTexture.height, 1, 1, null);
+		this.textureSurface.isNPOT = true;
+		// create an image to hold the surface
+		this.textureImage = new imageClass();
+		// _surface, _cellFrame, _anchorX, _anchorY, _tiling, _fullScreen
+		this.textureImage.create(this.textureSurface, 0, 0, 0, false, false);
+		// create a sprite to hold the image
+		this.textureSprite = new pbSprite();
+		// _image, _x, _y, _z, _angleInRadians, _scaleX, _scaleY
+		this.textureSprite.create(this.textureImage, 0, 0, 1, 0, 1.0, 1.0);
+		this.tx = 0;
+		this.tdx = 3;
+		this.ty = 300;
+		this.tdy = 2;
+		this.tr = 0;
+		this.tdr = 0.01;
+		// create a transform matrix to draw this image with
+		this.transform = pbMatrix3.makeTransform(this.tx, this.ty, this.tr, 0.7, -0.7);
 
 	    // clear the gl bindings
 	    gl.bindRenderbuffer(gl.RENDERBUFFER, null);
 	    gl.bindTexture(gl.TEXTURE_2D, null);
 	    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
 
+    	// set up the renderer postUpdate callback to draw the rendered scene from the RAM surface to the display
+	    this.renderer.postUpdate = this.postUpdate;
+
 		// don't do this again...
 		this.firstTime = false;
 	}
 
-	// draw the scene using the render-to-texture framebuffer
-	this.drawSceneToTexture(this.rttFramebuffer);
+	// update the invaders demo core
+	this.game.update();
+
+	// set the frame buffer to be used as the destination during the draw phase of renderer.update
+   	this.renderer.useFramebuffer = this.rttFramebuffer;
+};
+
+
+pbCameraRTTDemo.prototype.postUpdate = function()
+{
+//	gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+//	this.renderer.graphics.drawTextureToDisplay(0, this.rttTexture);
+
+	// get the scene we just drew to the rttTexture into the prepared RAM surface
 	this.renderer.graphics.textures.prepareTextureForAccess(this.rttTexture);
 	this.renderer.graphics.textures.getTextureToSurface(this.textureSurface);
-	gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+	// make sure the new surface contents are uploaded to the GPU when it's time to draw
+	this.textureSurface.image.isDirty = true;
 
-	// draw the texture surface
-	this.renderer.graphics.drawTextureToDisplay(0, this.rttTexture);
+	// move the draw image around
+	this.tx += this.tdx;
+	if (this.tx <= 0 || this.tx >= pbRenderer.width) this.tdx = -this.tdx;
+	this.ty += this.tdy;
+	if (this.ty <= 0 || this.ty >= pbRenderer.height) this.tdy = -this.tdy;
+	this.tr += this.tdr;
+	if (this.tr >= Math.PI * 2.0) this.tr -= Math.PI * 2.0;
+
+	this.transform = pbMatrix3.makeTransform(this.tx, this.ty, this.tr, 0.7, -0.7);
+
+	// draw the sprite holding the RAM surface to the visible display
+	gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+	this.renderer.graphics.drawImageWithTransform( this.textureImage, this.transform, 0.0 );
 };
+
