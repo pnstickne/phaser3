449adc6d3b4ab51b24ee81e4475583198291a588
Pete Baron <sibaroni@hotmail.com>
2015-05-26 14:59:16 +1200

    Add a 'contains' method to pbRectangle.

--- renderer/src/data_structures/pbRectangle.js
+++ renderer/src/data_structures/pbRectangle.js
@@ -14,4 +14,16 @@ function pbRectangle(x, y, width, height)
 	this.height = height;
 }
 
+pbRectangle.prototype.constructor = pbRectangle;
+
+
+pbRectangle.prototype.contains = function(_x, _y)
+{
+	// include left edge, exclude right edge
+	if (_x < this.x) return false;
+	if (_y < this.y) return false;
+	if (_x >= this.x + this.width) return false;
+	if (_y >= this.y + this.height) return false;
+	return true;
+};
 
