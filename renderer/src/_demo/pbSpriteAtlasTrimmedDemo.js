fd1f913efedf02fce8bac5177a14f7e09b768ab8
Pete Baron <sibaroni@hotmail.com>
2015-07-17 14:16:18 +1200

    Test single sprite surface trimming to ensure that the sprite offsets work correctly.

--- renderer/src/_demo/pbSpriteAtlasTrimmedDemo.js
+++ renderer/src/_demo/pbSpriteAtlasTrimmedDemo.js
@@ -22,6 +22,7 @@ function pbSpriteAtlasTrimmedDemo( docId )
 	pbPhaserRender.loader.loadImage( "original", "../img/spriteAtlas/test_original.png", 64, 64, 2, 2 );
 	this.ball = pbPhaserRender.loader.loadImage( "ball", "../img/spriteAtlas/test.png" );
 	this.ballJSON = pbPhaserRender.loader.loadFile( "../img/spriteAtlas/test.json" );
+	this.ballSingle = pbPhaserRender.loader.loadImage( "ball1", "../img/spriteAtlas/ballSingle.png" );
 
 	console.log( "pbSpriteAtlasTrimmedDemo c'tor exit" );
 }
@@ -47,9 +48,14 @@ pbSpriteAtlasTrimmedDemo.prototype.create = function()
 	// initialise the surface using the loaded image and the atlas JSON
 	this.surface.createAtlas(jsonString, pbPhaserRender.loader.getFile( this.ball ));
 
+	// create a 'single' sprite surface to test the trimming offset parameters
+	this.single = new pbSurface();
+	//_imageData, _rttTexture, _rttTextureRegister, _trimmedFrom, _offsets)
+	this.single.createSingle(pbPhaserRender.loader.getFile( this.ballSingle ), undefined, undefined, { width: 100, height:100 }, { x: 50 - 16, y: 100 - 16 });
+
 	this.original = new pbSprite();
 	this.original.createWithKey(200, 300, 'original', rootLayer);
-	this.original.anchorX = 1.0;
+	this.original.anchorX = 0.5;
 	this.original.anchorY = 0.5;
 
 	this.addSprites();
@@ -61,6 +67,8 @@ pbSpriteAtlasTrimmedDemo.prototype.destroy = function()
 	console.log("pbSpriteAtlasTrimmedDemo.destroy");
 
 	this.surface.destroy();
+	this.single.destroy();
+	this.original.destroy();
 	this.ball = null;
 	this.ballJSON = null;
 	this.list = null;
@@ -85,20 +93,32 @@ pbSpriteAtlasTrimmedDemo.prototype.addSprites = function()
 	console.log("pbSpriteAtlasTrimmedDemo.addSprites");
 
 	this.list = [];
-	for(var i = 0; i < 2; i++)
+	var i = 0;
+	for(; i < 2; i++)
 	{
+		// add multiple instances of this atlas trimmed surface as transform objects
 		var img = new imageClass();
-		img.create(this.surface, 0, 1.0, 0.5);
-
+		img.create(this.surface, 0, 0.5, 0.5);
 		var obj = new pbTransformObject();
 		obj.create(img, this.original.x + 64 * (i + 1), this.original.y, 1.0, 0, 1, 1);
 		rootLayer.addChild(obj);
-
 		this.list[i] = {
 			transformObj: obj,
 			dir: 0.1
 		};
 	}
+
+	// add a single instance of the trimmed single surface as a transform object
+	var img2 = new imageClass();
+	img2.create(this.single, 0, 0.5, 0.5);
+	var obj2 = new pbTransformObject();
+	obj2.create(img2, this.original.x + 64 * (i + 1), this.original.y, 1.0, 0, 1, 1);
+	rootLayer.addChild(obj2);
+	this.list[i++] = {
+		transformObj: obj2,
+		dir: 0.1
+	};
+
 };
 
 
@@ -110,6 +130,7 @@ pbSpriteAtlasTrimmedDemo.prototype.update = function()
 	{
 		obj = this.list[i].transformObj;
 
+		// advance the animation cell number
 		obj.image.cellFrame += this.list[i].dir;
 		if (obj.image.cellFrame >= obj.image.surface.cells)
 		{
@@ -121,10 +142,13 @@ pbSpriteAtlasTrimmedDemo.prototype.update = function()
 		{
 			obj.image.cellFrame = 0;
 			this.list[i].dir = -this.list[i].dir;
+			// rotate once each time the animation restarts at the beginning
+			// NOTE: for the single sprite this will rotate every time it attempts to animate
 			obj.angleInRadians += 0.1;
 		}
 	}
 
+	obj = this.list[0].transformObj;
 	if (obj)
 	{
 		// make the original sprite match the animation and rotation of the reconstructed atlas version
